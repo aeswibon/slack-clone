a29a5104c384fe678c59eec2b5f5b4888555b12c
cp-Coder <cplusplusCoder.vnit@gmail.com>
2021-08-12 16:03:21 +0530

    Initial commit

---
+++ src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+import { auth, provider } from "../Firebase/firebase";
+import { useStateValue } from "../Utility/StateProvider";
+import { actionTypes } from "../Utility/reducer";
+
+function Login() {
+  const [state, dispatch] = useStateValue();
+
+  const signIn = (error) => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: result.user,
+        });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <div className="login">
+      <div className="login_container">
+        <img src="../images/logo.png" alt="slack-logo"/>
+        <h1>Sign in to Slack</h1>
+        <Button variant="outlined" color="primary" onClick={signIn}>
+          Sign In with Google
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
